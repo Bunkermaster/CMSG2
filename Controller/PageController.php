608e289daf90961d71676acc3bbe12167309e8a8
Ikimasoft <yann@ikimasoft.fr>
2016-05-13 09:39:48 +0200

    composer, controller et model

---
+++ Controller/PageController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Controller;
+
+/**
+ * Class PageController
+ * @author Yann Le Scouarnec <yann.le-scouarnec@hetic.net>
+ * @package Controller
+ */
+class PageController
+{
+   public function ajoutAction()
+   {
+   }
+
+   public function supprimerAction()
+   {
+   }
+
+   public function modifierAction()
+   {
+   }
+
+   public function detailsAction()
+   {
+   }
+
+   public function listeAction()
+   {
+   }
+
+   public function displayAction()
+   {
+   }
+
+}
---
+++ Model/Pagerepository.php
@@ -0,0 +1,18 @@
+<?php
+namespace Model;
+
+/**
+ * Class Pagerepository
+ * @author Yann Le Scouarnec <yann.le-scouarnec@hetic.net>
+ * @package Model
+ */
+class Pagerepository
+{
+    /**
+     * @param int $id
+     */
+    public function display($id)
+    {
+        
+    }
+}
