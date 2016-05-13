a3a6431862836a46739c378b65086e404f4fb5b2
Ikimasoft <yann@ikimasoft.fr>
2016-05-13 10:16:21 +0200

    premiere route: display

---
+++ View/404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 lol</title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+</head>
+<body role="document">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container theme-showcase" role="main">
+    <div class="jumbotron">
+        <h1>HAHAHHAHA TU t'es perdu</h1>
+    </div>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yann
- * Date: 13/05/16
- * Time: 09:41
- */
+require_once "vendor/autoload.php";
+try{
+    $pdo = new \PDO("mysql:host=localhost;dbname=kandt","root","root");
+    $pdo->query('set NAMES \'utf8\'');
+} catch(PDOException $e){
+    die($e->getMessage());
+}
+$page = new \Controller\PageController($pdo);
+$page->displayAction();
